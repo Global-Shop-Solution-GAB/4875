365ac8068bacb0056b34d988c37593c22b08fa98
Service Web <sw@gssmail.com>
2024-03-12 17:25:18 -0500

    Commit changes in C:\Websites\Git\emarlow\4875\GCG_4875_TAX_ID.sql

---
+++ GCG_4875_TAX_ID.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_4875_TAX_ID
+ * ========================================== */
+CREATE TABLE "GCG_4875_TAX_ID"(
+ "CV_TYPE" CHAR(6),
+ "CV_ID" CHAR(6),
+ "CV_DIGIT" CHAR(2),
+ "CV_TAX_ID" CHAR(50));
+
+
